ad64181510d9eade6156145790d06e596d13b9c2
Rxfai24 <86569232+Rxfai24@users.noreply.github.com>
2021-07-08 23:15:40 +0530

    Add files via upload

---
+++ knightingale/Basic.java
@@ -0,0 +1,20 @@
+package knightingale;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        //Data declaration...
+		int i=100;
+        int j=200;
+        
+        //Processing of Data
+        
+        int sum=i+j;
+        
+        //output
+        
+        System.out.println("The result is..."+sum);
+	}
+
+}
---
+++ knightingale/VariableDEMO.java
@@ -0,0 +1,28 @@
+package knightingale;
+public class VariableDEMO {
+	public static void main(String[] args) {
+		//variable valid identifiers
+		
+		// a valid identifier always starts with a character or either of these two special characters _, $ and in between u can have a number
+		int age;
+		int _age;
+		int $age;
+		int _age_;
+		int _$a_$g;
+		int a2g2e;
+		
+		//invalid identifiers
+		
+		//int 2age;// you cannot start a variable with a number
+	 //	int @#@age;//you cannot start a variable name with special characters other than $ and _
+	//	int a g e;//you cannot have space in between the names
+		
+		//INITIALIZATION OF VARIABLES
+		
+		int i=100;//valid value to initialize 32 bit
+		int ii=21_47_48_3_647;
+		short s=2323;  //16 bit
+		short ss=32767;
+		byte b=10;  // 8 bit
+		byte bb= -126
+		
